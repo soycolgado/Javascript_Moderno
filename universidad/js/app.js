9e057b5e228a2c254ae8ddbba391953b9fcc9f27
Nestor Bardel <soycolgado@gmail.com>
2020-05-12 02:05:43 -0300

    empezando las practicas

---
+++ universidad/js/app.js
@@ -0,0 +1,17 @@
+// let imagenes = document.images;
+// // console.log(imagenes);
+// let imagenesArr = Array.from(imagenes);
+// // console.log(imagenesArr);
+// imagenesArr.forEach((imagen)=>{
+//     console.log(imagen);
+// });
+
+//getElementbyId()
+let elemento;
+// elemento = document.getElementById('hero');
+elemento = document.getElementById('encabezado');
+// console.log(elemento.textContent);
+elemento.style.background = "#333";
+elemento.style.color = "#fff";
+elemento.style.padding = "20px";
+elemento.innerText = "Los mejores cursos";
